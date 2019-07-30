ba564311b5dd07f18af155bb305212add01d611d
IKuseal <sergeykudashkin1999@gmail.com>
2019-07-30 12:33:56 +0300

    trans-worker was resulted to full work condition

--- word-tra-worker.js
+++ word-tra-worker.js
@@ -1,26 +1,33 @@
-const {workerData} = require('worker_threads');
+//const {workerData} = require('worker_threads');
 
 var needle = require('needle');
 const cheerio = require('cheerio');
-
-needle.get(workerData.source + workerData.word,function(error,respone){
+//workerData.source + workerData.word
+needle.get("https://wooordhunt.ru/word/green",function(error,respone){
     if(!error){
         const $ = cheerio.load(respone.body);
         var translations = [];
+        var numOfTra = 0;
+        var numOfHidden;
         console.log("star");
         for(var i = 0; i<$('div.tr').length;++i){
-            //console.log($('div.tr').eq(i).children("span").length);
-            for(var j = 0; j<$('div.tr').eq(i).children("span").length;++j){
-                translations[j] = $('div.tr').eq(i).children("span").eq(j).text();
+            //console.log("NumOfChildren:"+$('div.tr').eq(i).children().length);
+            
+            for(var j = 0; j<$('div.tr').eq(i).children("span").length-1;++j){
+                translations[numOfTra] = $('div.tr').eq(i).children("span").eq(j).text();
+                numOfTra++;
             }
-            // if($('div.tr').eq(i).children('span.more').length){
-            //     for(var j=0;j<$('div.tr').eq(i).children('span.more').children("span").length;++j){
-            //         console.log($('div.tr').eq(i).children('span.more').children("span").eq(j).text());
-            //     }
-            // }
+            //console.log("hidden:"+$('div.tr').eq(i).children("div.hidden").children("span").length);
+            numOfHidden = $('div.tr').eq(i).children("div.hidden").length;
+            for(var j =0; j<$('div.tr').eq(i).children("div.hidden").eq(numOfHidden-1).children("span").length;++j){
+                translations[numOfTra] = $('div.tr').eq(i).children("div.hidden").eq(numOfHidden-1).children("span").eq(j).text();
+                ++numOfTra;
+            }
+            
         }
+
         for(var i = 0; i<translations.length;++i){
-            console.log(translations[i]);
+            console.log(i+translations[i]);
         }
         console.log("end");
         
