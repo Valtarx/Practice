5a6b6ed037d0b2da634e4605c15ea02668c416d8
IKuseal <sergeykudashkin1999@gmail.com>
2019-07-31 00:27:38 +0300

    new step in making def-worker for dictionary.cambridge

--- def-worker.js
+++ def-worker.js
@@ -14,7 +14,22 @@ else{
         var examples = [];
         const $ = cheerio.load(body);
         console.log("star");
-        console.log($("div#dataset-cald4.dictionary").text());
+        //console.log(ukDef.find("div.entry-body").children().length);
+        var ukDef = $("div#dataset-cald4.dictionary");
+        var partOfSpeech;
+        var senseBlock;
+        for(var i =0; i<ukDef.find("div.entry-body").children().length;++i){
+            partOfSpeech = ukDef.find("div.entry-body").children().eq(i);
+            for(var i1 = 0;i1<partOfSpeech.children("div.pos-body").children("sense-block").length;++i1){
+                senseBlock = partOfSpeech.children("div.pos-body").children("sense-block").eq(i1);
+                if(i==0 && i1==0){
+                    console.log(senseBlock.text());
+                }
+            }
+        }
+        console.log($("div#dataset-cald4.dictionary").find("div.entry-body").text());
+
+
 
 
         console.log("end");    
--- word-tra-worker.js
+++ word-tra-worker.js
@@ -9,36 +9,36 @@ if(0!=0){
 else{
     request('https://wooordhunt.ru/word/green', function (error, response, body) {
 
-            const $ = cheerio.load(body);
-            var translations = [];
-            var numOfTra = 0;
-            var numOfHidden;
-            console.log("star");
-            for(var i = 0; i<$('div.tr').length;++i){
-                //console.log("NumOfChildren:"+$('div.tr').eq(i).children().length);
-                
-                for(var j = 0; j<$('div.tr').eq(i).children("span").length-1;++j){
-                    translations[numOfTra] = $('div.tr').eq(i).children("span").eq(j).text();
-                    numOfTra++;
-                }
-                //console.log("hidden:"+$('div.tr').eq(i).children("div.hidden").children("span").length);
-                numOfHidden = $('div.tr').eq(i).children("div.hidden").length;
-                for(var j =0; j<$('div.tr').eq(i).children("div.hidden").eq(numOfHidden-1).children("span").length;++j){
-                    translations[numOfTra] = $('div.tr').eq(i).children("div.hidden").eq(numOfHidden-1).children("span").eq(j).text();
-                    ++numOfTra;
-                }
-                
+        const $ = cheerio.load(body);
+        var translations = [];
+        var numOfTra = 0;
+        var numOfHidden;
+        console.log("star");
+        for(var i = 0; i<$('div.tr').length;++i){
+            //console.log("NumOfChildren:"+$('div.tr').eq(i).children().length);
+            
+            for(var j = 0; j<$('div.tr').eq(i).children("span").length-1;++j){
+                translations[numOfTra] = $('div.tr').eq(i).children("span").eq(j).text();
+                numOfTra++;
             }
-    
-            for(var i = 0; i<translations.length;++i){
-                console.log(i+translations[i]);
+            //console.log("hidden:"+$('div.tr').eq(i).children("div.hidden").children("span").length);
+            numOfHidden = $('div.tr').eq(i).children("div.hidden").length;
+            for(var j =0; j<$('div.tr').eq(i).children("div.hidden").eq(numOfHidden-1).children("span").length;++j){
+                translations[numOfTra] = $('div.tr').eq(i).children("div.hidden").eq(numOfHidden-1).children("span").eq(j).text();
+                ++numOfTra;
             }
             
-    
-            console.log("end");
-            
-    
+        }
+
+        for(var i = 0; i<translations.length;++i){
+            console.log(i+translations[i]);
+        }
         
-    })
+
+        console.log("end");
+        
+
+    
+})
 }
 
