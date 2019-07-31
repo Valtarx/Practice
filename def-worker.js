902b3b3a8f3fab31f7674678ea47403ae22aad77
IKuseal <sergeykudashkin1999@gmail.com>
2019-07-31 15:55:30 +0300

    added the support of lexico.com

--- def-worker.js
+++ def-worker.js
@@ -1,5 +1,4 @@
 const {workerData} = require('worker_threads');
-
 const cheerio = require('cheerio');
 var request = require('request');
 
@@ -7,44 +6,107 @@ if(0!=0){
 
 }
 else{
-    request(workerData.source + workerData.word, function (err, res, body) {
+    var site;
+    switch(workerData.source){
+        case "https://dictionary.cambridge.org":
+            site = workerData.source+"/dictionary/english/"+workerData.word;
+            break;
+        case "https://www.lexico.com":
+            site = workerData.source+"/en/definition/"+workerData.word;
+            break;
+        default:
+            break;
+    }
+    request(site, function (err, res, body) {
         
         const $ = cheerio.load(body);
         console.log("star");
         var definitions = [];
         var examples = [];
-        var partOfSpeech;
-        var definitionBlock;
-        var definition;
-        var examplesBlock;
-        var example;
         var numOfDef =0;
         const minNumOfExamples = 3;
+        var partOfSpeech;
+        var definitionBlock;
+        var examplesBlock;
+        var definition;
+        var example;
         var min;
         var j;
-        var ukDef = $("div#dataset-cald4.dictionary");
-        for(var i =0; i<ukDef.find("div.entry-body").children().length;++i){
-            partOfSpeech = ukDef.find("div.entry-body").children().eq(i);
-            for(var i1 = 0;i1<partOfSpeech.find("div.pos-body").children("div.sense-block").length;++i1){
-                definitionBlock = partOfSpeech.children("div.pos-body").children("div.sense-block").eq(i1);
-                definitionBlock = definitionBlock.children("div.sense-body");
-                definitionBlock = definitionBlock.children("div.def-block.pad-indent");
-                definition = definitionBlock.children("p.def-head.semi-flush");
-                definition = definition.children("b.def");
-                definitions[numOfDef] = definition;
-                ++numOfDef;
-                examplesBlock = definitionBlock.children("span.def-body");
-                min = minNumOfExamples<examplesBlock.children("div.examp.emphasized").length ? minNumOfExamples:examplesBlock.children("div.examp.emphasized").length;
-                j=0;
-                while(j<min){
-                    example = examplesBlock.children("div.examp.emphasized").eq(j);
-                    examples[(numOfDef-1)*minNumOfExamples+j] = example.text();
-                    ++j;
-                }
-            }
+        switch(workerData.source){
+            case "https://dictionary.cambridge.org":
+                          
+                    var ukDef = $("div#dataset-cald4.dictionary");
+                    for(var i =0; i<ukDef.find("div.entry-body").children().length;++i){
+                        partOfSpeech = ukDef.find("div.entry-body").children().eq(i);
+                        for(var i1 = 0;i1<partOfSpeech.find("div.pos-body").children("div.sense-block").length;++i1){
+                            definitionBlock = partOfSpeech.children("div.pos-body").children("div.sense-block").eq(i1);
+                            definitionBlock = definitionBlock.children("div.sense-body");
+                            definitionBlock = definitionBlock.children("div.def-block.pad-indent");
+                            definition = definitionBlock.children("p.def-head.semi-flush");
+                            definition = definition.children("b.def");
+                            definitions[numOfDef] = definition.text();
+                            ++numOfDef;
+                            examplesBlock = definitionBlock.children("span.def-body");
+                            min = minNumOfExamples<examplesBlock.children("div.examp.emphasized").length ? minNumOfExamples:examplesBlock.children("div.examp.emphasized").length;
+                            j=0;
+                            while(j<min){
+                                example = examplesBlock.children("div.examp.emphasized").eq(j);
+                                examples[(numOfDef-1)*minNumOfExamples+j] = example.text();
+                                ++j;
+                            }
+                        }
+                    }
+                    break;
+                case "https://www.lexico.com":
+                    console.log("lexico");
+                    var partOfSpeechBlock = $("div.entryWrapper");
+                    var extraExamplesBlock;
+                    var exLen1;
+                    var exLen2;
+                    var j1 =0;
+                    for(var i =0;i<partOfSpeechBlock.children("section.gramb").length;++i){
+                        partOfSpeech = partOfSpeechBlock.children("section.gramb").eq(i);
+                        partOfSpeech = partOfSpeech.children("ul.semb");
+                        for(var i1 =0;i1<partOfSpeech.children("li").length;++i1){
+                            definitionBlock = partOfSpeech.children("li").eq(i1);
+                            definitionBlock = definitionBlock.children("div.trg");
+                            definition = definitionBlock.children("p").children("span.ind").text();
+                            definitions[numOfDef] = definition;
+                            ++numOfDef;
+
+                            examplesBlock = definitionBlock;
+                            extraExamplesBlock = examplesBlock.children("div.examples");
+                            extraExamplesBlock = extraExamplesBlock.children("div.exg");
+                            extraExamplesBlock = extraExamplesBlock.children("ul.english-ex");
+                            exLen1 = examplesBlock.children("div.exg").length;
+                            exLen2 = extraExamplesBlock.children("li.ex").length
+                            min = minNumOfExamples<exLen1+exLen2 ? minNumOfExamples:exLen1+exLen2;
+                            j = 0;
+                            j1= 0;
+                            while(j<min){
+                                if(j<exLen1){
+                                    example = examplesBlock.children("div.exg").eq(j).text();
+                                    examples[(numOfDef-1)*3+j] = example;
+                                }
+                                else{
+                                    example = extraExamplesBlock.children("li.ex").eq(j1).text();
+                                    examples[(numOfDef-1)*3+j] = example;
+                                    ++j1;
+                                }
+                                ++j;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
         }
+
+        
+
+
         for(var i =0;i<definitions.length;++i){
-            console.log(definitions[i].text());
+            console.log(definitions[i]);
         }
         console.log("Examples");
         for(var i=0;i<numOfDef*minNumOfExamples;++i){
@@ -69,6 +131,6 @@ else{
 
         console.log("end");    
     });
-    
+
    console.log("end2");
 }
