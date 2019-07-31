850728576baba5da108ea760de3dde133d23db42
IKuseal <sergeykudashkin1999@gmail.com>
2019-07-31 16:46:49 +0300

    added some things to connect with workers

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Pool = require('worker-threads-pool');
+const pool = new Pool({max: 6})
 var amqp = require('amqplib/callback_api');
 
 const exapp = express();
@@ -8,7 +9,8 @@ var isResponseReadyTimer;
 var isResponseReadyTimers = [];
 var isRepsonseReadyFlags  = [];
 var totalNumOfResponses = -1;
-
+var timer1;
+var timer2;
 exapp.get("/translations",function(request,response){
   amqp.connect('amqp://localhost', function(error0, connection) {
     if (error0) {
@@ -24,11 +26,16 @@ exapp.get("/translations",function(request,response){
         }
         var queue = 'translation_queue';
                 
-        var msg = indexOfThisResponse.toString()+";https://wooordhunt.ru/word/;"+"green";
+        var source = "https://wooordhunt.ru"; 
+        var word   = "green";
+        var msg = indexOfThisResponse.toString()+";"+source+";"+word;
 
         channel.assertQueue(queue, {
             durable: true
         });
+        if(timer1 == undefined)
+          timer1 = setInterval(giveTask,1000,{queueName:queue,fileName:'./tra-worker.js'});
+        
         channel.sendToQueue(queue, Buffer.from(msg), {
             persistent: true
         });
@@ -61,12 +68,18 @@ exapp.get("/definitions",function(request,response){
             throw error1;
         }
         var queue = 'definitions_queue';
-        //поставить другой сайт для дефиниций        
-        var msg = indexOfThisResponse.toString()+";https://wooordhunt.ru/word/;"+"green";
+        //поставить другой сайт для дефиниций
+        var source = "https://dictionary.cambridge.org"; 
+        //source = "https://www.lexico.com";
+        var word   = "green";
+        var msg = indexOfThisResponse.toString()+";"+source+";"+word;
 
         channel.assertQueue(queue, {
             durable: true
         });
+        if(timer2 == undefined)
+          timer2 = setInterval(giveTask,1000,{queueName:queue,fileName:'./def-worker.js'});
+        
         channel.sendToQueue(queue, Buffer.from(msg), {
             persistent: true
         });
@@ -90,21 +103,15 @@ exapp.listen(3000,"127.0.0.1",function(){
 })  
 
 
-
-const pool = new Pool({max: 6})
-var source ='https://wooordhunt.ru/word/';
-
-var word;
-
-var workerData = {source,word};
- 
-
 var i = 0;
 
-function giveTask(){
+function giveTask(typeOfTask){
+  var source;
+  var word;
+  var workerData = {source,word};
   amqp.connect('amqp://localhost', function(error, connection) {
     connection.createChannel(function(error, channel) {
-        var queue = 'translation_queue';
+        var queue = typeOfTask.queueName;
 
         console.log(" [*] Waiting for messages in %s.", queue);
         channel.consume(queue, function(msg) {
@@ -112,9 +119,9 @@ function giveTask(){
         var data = msg.content.toString().split(';');
         workerData.word = data[2];
         workerData.source = data[1];
-
+        
         ////////////////
-        pool.acquire('./word-tra-worker.js',{workerData} ,function (err, worker) {
+        pool.acquire(typeOfTask.fileName,{workerData} ,function (err, worker) {
           //pool.acquire('./ndl.js', function (err, worker) {
           if (err) throw err
           console.log(`started worker ${i} (pool size: ${pool.size})`)
@@ -143,6 +150,6 @@ function isResponseReady(response,indexOfThisResponse){
 
 
 
-var timer = setInterval(giveTask,1000);
+
 
 
